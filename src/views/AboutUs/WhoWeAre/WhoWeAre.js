3e3d397f339769e1f9029ce9426ac936693a1217
Harsh Dadhich <hdadhich01@gmail.com>
2023-07-29 15:19:01 +0530

    Add changes

--- src/views/AboutUs/WhoWeAre/WhoWeAre.js
+++ src/views/AboutUs/WhoWeAre/WhoWeAre.js
@@ -8,27 +8,34 @@ export default class WhoWeAre extends React.Component {
   };
 
   render() {
-    const { title, subtext, backgroundImage } = this.props;
-    const backgroundStyle = {
-      backgroundImage:
-        "https://eyespace.ai/wp-content/uploads/2022/10/OR-scaled.jpg",
-    };
-
     return (
-      <div className="who-we-are" style={backgroundStyle}>
-        <h1 className="title">Who We Are</h1>
-        <p className="subtext">
-          eyeSpace is a cross-platform Ophthalmology utility, social media,
-          networking service that provides users with a wide array of eye care
-          tools to optimize and track their surgical outcomes. eyeSpace was
-          conceptualized by Matt Hirabayashi MD and Gurpal Virdi MD and
-          technological development is done through an engineering team at
-          EyeLabs. The founders have extensive experience with ocular surgery,
-          technological development, scaling products, overseeing projects, and
-          managing teams. eyeSpace is a private entity with no financial
-          relationship to any of the manufacturers whose data is archived.
-          Neither of the founders have conflicts of interest.
-        </p>
+      <div className="WhoWeAre">
+        <ul>
+          <img
+            className="background-image"
+            src="https://i.ibb.co/BPMX4SC/OR-scaled.jpg"
+          />
+        </ul>
+        <div className="text-overlay">
+          <h2 className="title">Who We Are</h2>
+          <p className="subtext">
+            eyeSpace is a cross-platform Ophthalmology utility, social media,
+            networking service that provides users with a wide array of eye care
+            tools to optimize and track their surgical outcomes.
+            <br />
+            <br />
+            eyeSpace was conceptualized by Matt Hirabayashi MD and Gurpal Virdi
+            MD and technological development is done through an engineering team
+            at EyeLabs. The founders have extensive experience with ocular
+            surgery, technological development, scaling products, overseeing
+            projects, and managing teams.
+            <br />
+            <br />
+            eyeSpace is a private entity with no financial relationship to any
+            of the manufacturers whose data is archived. Neither of the founders
+            have conflicts of interest.
+          </p>
+        </div>
       </div>
     );
   }
